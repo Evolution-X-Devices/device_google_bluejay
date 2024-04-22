c44661447ee6cabe0fa5980d897021ea56067d63
AnierinB <anierin@evolution-x.org>
2024-04-22 13:54:00 -0700

    bluejay: Initialize for Evolution X 11.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- lineage_bluejay.mk
+++ lineage_bluejay.mk
@@ -7,11 +7,17 @@
 # Inherit some common stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit some common Evolution X stuff
+TARGET_DISABLE_EPPE := true
+
 # Inherit device configuration
 DEVICE_CODENAME := bluejay
 DEVICE_PATH := device/google/bluejay
 VENDOR_PATH := vendor/google/bluejay
 $(call inherit-product, $(DEVICE_PATH)/aosp_$(DEVICE_CODENAME).mk)
+
+$(call inherit-product, device/google/gs101/evolution_common.mk)
+$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-evolution.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
